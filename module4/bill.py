b40e5d9e22d968709e3407bdae9b0554e0ad44e6
Veera03122005 <saimaneendra3125@gmail.com>
2026-06-14 20:59:52 +0530

    Describe your changes

---
+++ module4/bill.py
@@ -0,0 +1,14 @@
+"""Write a program that asks for a product price and quantity, then prints a neatly 
+formatted bill showing item total, a 5% tax amount, and the grand total - all aligned and 
+formatted to 2 decimals using f-strings."""
+product_price = float(input("Enter the product price: "))
+quantity = int(input("Enter the quantity: "))
+print("******************Bill************************")
+tax_rate = 0.05
+total_bill = product_price * quantity
+total_including_tax = total_bill + (total_bill * tax_rate)
+print(f"Item Total:       {quantity}")
+print(f"Total bill:       {total_bill:.2f}")
+print(f"Tax Amount(5%):   {total_bill * tax_rate:.2f}")
+print(f"Grand Total:      {total_including_tax:.2f}")
+print("*********************X************************")
